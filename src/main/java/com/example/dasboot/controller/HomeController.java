632ac44a74373d8beba75faa0924cff78f240e9c
hewell <hewell@gmail.com>
2019-06-09 16:37:47 +0800

    hello world home

---
+++ src/main/java/com/example/dasboot/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.dasboot.controller;
+
+import java.lang.invoke.MethodHandles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+  private final static Logger LOGGER = LoggerFactory
+      .getLogger(MethodHandles.lookup().lookupClass());
+
+  @RequestMapping("/")
+  public String home(){
+    return "Hello World!";
+  }
+}
